e2c3b173f8c105d9e80aa339b9e75c4cea5d87c8
mRcHiPs <Gulzar72441@gmail.com>
2021-11-05 15:31:29 +0500

    add  mvc

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+        public string GetBooks(int id)
+        {
+            return $"Book with id = {id}";
+        }
+        public string SearchBooks( string bookName, string authorName)
+        {
+            return $"Book with Name ={bookName} & Author ={authorName}";
+        }
+    }
+}
---
+++ BookStore/BookStore/Repository/BookRespository.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRespository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById (int id)
+        {
+            return DataSource().Where(ex => ex.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(ex => ex.Title.Contains(title)  && ex.Author.Contains(authorName)).ToList();
+
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id = 1, Title="C++",Author= "Gulzar"},
+                new BookModel(){Id = 2, Title="C#",Author= "Gulzar"},
+                new BookModel(){Id = 3, Title="MVC",Author= "Gulzar"},
+                new BookModel(){Id = 4, Title="JAVA",Author= "Gulzar"},
+                new BookModel(){Id = 5, Title="Python",Author= "Gulzar"},
+                new BookModel(){Id = 6, Title="C",Author= "Gulzar"},
+                new BookModel(){Id = 7, Title="Asp.net",Author= "Gulzar"},
+                new BookModel(){Id = 8, Title="PHP",Author= "Gulzar"},
+                new BookModel(){Id = 9, Title="F#",Author= "Gulzar"},
+                new BookModel(){Id = 10, Title="VB",Author= "Gulzar"},
+            };
+        }
+    }
+}
